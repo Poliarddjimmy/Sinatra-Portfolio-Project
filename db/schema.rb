aff99565d7346d17ead52ebd4ada5ade9005bdef
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-18 06:09:59 +0000

    rename column password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190618045616) do
+ActiveRecord::Schema.define(version: 20190618060817) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "user_id"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20190618045616) do
     t.string  "last_name"
     t.string  "pseudo"
     t.string  "email"
-    t.string  "password"
+    t.string  "password_digest"
     t.integer "is_a_teacher"
   end
 
