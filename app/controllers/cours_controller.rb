bdc12dd8c44eb5dc53217cd06b387de179a11df7
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-18 10:33:10 +0000

    change the name of the table courses

--- app/controllers/cours_controller.rb
+++ app/controllers/cours_controller.rb
@@ -4,6 +4,7 @@ require 'rack-flash'
 class CoursController < ApplicationController
   get "/cours" do
     if Helpers.is_logged_in? session
+      @user = Helpers.current_user session
       erb :'/cours/cours'
     else
       redirect to '/'
