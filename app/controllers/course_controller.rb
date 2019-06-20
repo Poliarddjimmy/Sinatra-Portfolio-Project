09428761e4b07d8a92919acdbc283c93cd7cb7b9
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-20 05:09:20 +0000

    make change

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,35 +1,46 @@
 class CourseController < ApplicationController
-  get "/cours" do
+  get "/course" do
     if Helpers.is_logged_in? session
       @user = Helpers.current_user session
-      erb :'/cours/cours'
+      erb :'/course/cours'
     else
       redirect to '/'
     end
   end
 
 
-  get "/cours/new" do
+  get "/course/new" do
     if Helpers.is_logged_in? session
       @user = Helpers.current_user session
-      erb :'/cours/new'
+      erb :'/course/new'
     else
       redirect to '/'
     end
   end
 
 
-  post "/cours/new" do
+  post "/course/new" do
     if Helpers.is_logged_in? session
 
       cour = Helpers.current_user(session).courses.create(:course_name=> params[:cours_name], :course_description => params[:cours_description])
       if cour.save
-        redirect to "/cours/#{cour.id}"
+        redirect to "/course/#{cour.id}"
       else
-        redirect to "/cours/new"
+        redirect to "/course/new"
       end
     else
       redirect to '/'
+    end
+  end
+
+
+  get '/course/:id' do
+    if Helpers.is_logged_in? session
+      @course = Course.find_by_id(params[:id])
+      @user = Helpers.current_user session
+      erb :'course/show'
+    else
+      redirect to '/login'
     end
   end
 
