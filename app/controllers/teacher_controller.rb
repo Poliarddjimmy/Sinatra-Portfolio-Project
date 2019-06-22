1139ac48eb0bb9257e85c3863c5547c80aa4c3b4
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-22 19:51:16 +0000

    Add option to create a teacher

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -2,9 +2,27 @@ require 'sinatra/base'
 require 'rack-flash'
 
 class TeachersController < ApplicationController
-  #use Rack::Flash
+  use Rack::Flash
 
-
-
+  patch '/teacher/new' do
+    if Helpers.is_logged_in? session
+      user = User.find_by(email: params[:email]) unless params[:email].empty?
+      if !user
+        user = User.new params
+        if user.save
+          flash[:teacher] = 'The teacher has been created'
+          redirect back
+        else
+          flash[:teacher] = 'Have an error in your process'
+          redirect back
+        end
+      else
+        flash[:teacher] = 'this email exist already. Please choose another one'
+        redirect back
+      end
+    else
+      redirect to '/'
+    end
+  end
 
 end
