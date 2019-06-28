e5f098f884ac7d854698269e37f49689fd56251b
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-28 18:16:59 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190628164657) do
+ActiveRecord::Schema.define(version: 20190628180135) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20190628164657) do
     t.string   "cr_description"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "class_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "class_id"
   end
 
   create_table "course_users", force: :cascade do |t|
